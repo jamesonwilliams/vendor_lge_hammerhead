1a7eedfc5a14351f430f4eb37bc029527f0a64b5
Jameson Williams <jameson.h.williams@intel.com>
2013-11-15 14:52:51 -0800

    Add hooks for Google apps package
    Signed-off-by: Jameson Williams <jameson.h.williams@intel.com>

--- device-vendor.mk
+++ device-vendor.mk
@@ -17,3 +17,4 @@ LOCAL_STEM := hammerhead/device-partial.mk
 $(call inherit-product-if-exists, vendor/broadcom/$(LOCAL_STEM))
 $(call inherit-product-if-exists, vendor/lge/$(LOCAL_STEM))
 $(call inherit-product-if-exists, vendor/qcom/$(LOCAL_STEM))
+$(call inherit-product-if-exists, vendor/google/gapps/device-partial.mk)
